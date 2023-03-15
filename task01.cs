1f4f7fcc2e541a00219e55e5451b3993aea1f71f
Benihimel <benihimekugutsu@gmail.com>
2023-03-15 17:04:25 -0400

    task01

---
+++ task01.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (1000 > num && num > 99)
+    Console.Write($"Third digit of number: {num % 100 / 10}");
+else
+    Console.Write("Error. Try again");
+
