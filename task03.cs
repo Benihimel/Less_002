2c83650af5b233133ad5841814f71fd3df8e59f1
Benihimel <127148808+Benihimel@users.noreply.github.com>
2023-03-16 17:28:48 -0400

    Add task03.cs

---
+++ task03.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход
+// цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 5 && number < 8)
+    Console.WriteLine("Yes, Day off");
+else if (0 < number && number < 6)
+    Console.WriteLine("No, Weekdays");
+else
+    Console.WriteLine("Incorrect number");
+
